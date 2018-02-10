c1925534c84a8e1d9ed9e186e4ab963c6b02016a
Deyan Vladkov <deian.vladkov@gmail.com>
2018-02-10 18:29:25 +0000

    Added venue details page

--- routes/routes.js
+++ routes/routes.js
@@ -26,154 +26,173 @@ if (!process.env.API_URL || (!process.env.API_ID) || (!process.env.API_KEY)) {
 
 module.exports = function (app, passport) {
 
-    app.get('/', function (req, res) {
-        if (req.isAuthenticated()) {
-            return res.render('index_auth');
-        }
-        res.render('index');
-    });
+app.get('/', function (req, res) {
+    if (req.isAuthenticated()) {
+        return res.render('index_auth');
+    }
+    res.render('index');
+});
 
-    app.post('/', function (req, res) {
-        if (!apiQuery) {
-            console.log('WARNING: Please export API credentials as environment variables !');
-            return res.send('ERROR: No API credentials exported !');
-        } else {
-            var apiResponse = {};
+app.post('/', function (req, res) {
+    if (!apiQuery) {
+        console.log('WARNING: Please export API credentials as environment variables !');
+        return res.send('ERROR: No API credentials exported !');
+    } else {
+        var apiResponse = {};
 
-            // TODO: Sanitaze user input
-            ajax.get(apiQuery + 'near=' + req.body.search).then(response => {
-                // Success
-                apiResponse = response.data.response;
+        // TODO: Sanitaze user input
+        ajax.get(apiQuery + 'near=' + req.body.search).then(response => {
+            // Success
+            apiResponse = response.data.response;
 
-            }).then(() => {
-                // Render results page                
-                res.render('search', {
-                    result: apiResponse,                    
-                });
-
-            }).catch(error => {
-                //console.log(error);
-                return res.send('Error fetching API data');
+        }).then(() => {
+            // Render results page                
+            res.render('search', {
+                result: apiResponse,
             });
-        }
-    });
-
-    app.get('/search', function (req, res) {
-        if (req.isAuthenticated()) {
-            return res.render('index_auth'); // TODO: auth search
-        }
-        res.render('search');
-    });
-
-    app.post('/venue/picture/:vid', function (req, res) {
-        var venueId = req.params.vid;
-        var venueUrl = "https://api.foursquare.com/v2/venues/" + venueId + "?client_id=" + config.api.client_id + "&client_secret=" + config.api.client_secret + "&v=" + config.api.v;
-
-        ajax.get(venueUrl).then(response => {
-            if (response.data.response.venue.bestPhoto) {
-                var pictureUrl = response.data.response.venue.bestPhoto.prefix + config.picture.resolution + response.data.response.venue.bestPhoto.suffix;
-                var jsonResponse = {
-                    url: pictureUrl
-                };
-            } else { // Picture not found
-                var jsonResponse = {
-                    url: config.picture.defaultVenuePicture
-                };
-            }
-            res.send(jsonResponse);
 
         }).catch(error => {
             //console.log(error);
+            return res.send('Error fetching API data');
+        });
+    }
+});
+
+app.get('/search', function (req, res) {
+    if (req.isAuthenticated()) {
+        return res.render('index_auth'); // TODO: auth search
+    }
+    res.render('search');
+});
+
+app.post('/venue/picture/:vid', function (req, res) {
+    var venueId = req.params.vid;
+    var venueUrl = "https://api.foursquare.com/v2/venues/" + venueId + "?client_id=" + config.api.client_id + "&client_secret=" + config.api.client_secret + "&v=" + config.api.v;
+
+    ajax.get(venueUrl).then(response => {
+        if (response.data.response.venue.bestPhoto) {
+            var pictureUrl = response.data.response.venue.bestPhoto.prefix + config.picture.resolution + response.data.response.venue.bestPhoto.suffix;
+            var jsonResponse = {
+                url: pictureUrl
+            };
+        } else { // Picture not found
             var jsonResponse = {
                 url: config.picture.defaultVenuePicture
             };
-            return res.send(jsonResponse);
-        });
-    });
-    
-    app.get('/venue/:vid', function (req, res) {
-        res.render('venue');
-    });
-
-    app.get('/login', function (req, res) {
-        res.render('login', {
-            message: req.flash('loginMessage')
-        });
-    });
-
-    app.post('/login', passport.authenticate('local-login', {
-            failureRedirect: '/login',
-            successRedirect: '/profile',
-            failureFlash: true // Allow flash messages
-        }),
-        function (req, res) {
-            if (req.body.remember) {
-                req.session.cookie.maxAge = 1000 * 60 * 3;
-            } else {
-                req.session.cookie.expires = false;
-            }
-            res.redirect('/');
-        });
-
-    app.get('/signup', function (req, res) {
-        res.render('signup', {
-            message: req.flash('signupMessage')
-        });
-    });
-
-    app.post(
-        '/signup',
-        // Form filter and validation        
-        form(
-            field("firstName").trim().required().is(/^[A-z]+$/),
-            field("lastName").trim().required().is(/^[A-z]+$/),
-            field("password").trim().required().len(8, 72, "Password must be between 8 and 72 characters"),
-            field("email").trim().required().isEmail(),
-            validate("rpassword").equals("field::password")
-        ),
-
-        // Express request-handler now receives filtered and validated data 
-        function (req, res) {
-            // Additional validations            
-            req.body.email = req.body.email.toLowerCase();
-
-            if (!req.form.isValid) {
-                // Handle errors 
-                //console.log(req.form.errors);
-                //TODO: flash messages
-                res.redirect('/signup');
-
-            } else {
-                passport.authenticate('local-signup', {
-                    successRedirect: '/login',
-                    failureRedirect: '/signup',
-                    failureFlash: true
-                })(req, res);
-            }
         }
-    );
+        res.send(jsonResponse);
 
-    app.get('/error', function (req, res) {
-        res.render('error', {
-            message: req.flash('errorMessage')
+    }).catch(error => {
+        //console.log(error);
+        var jsonResponse = {
+            url: config.picture.defaultVenuePicture
+        };
+        return res.send(jsonResponse);
+    });
+});
+
+app.get('/venue/:vid', function (req, res) {
+    var venueId = req.params.vid;
+    var venueUrl = "https://api.foursquare.com/v2/venues/" + venueId + "?client_id=" + config.api.client_id + "&client_secret=" + config.api.client_secret + "&v=" + config.api.v;
+    var apiResponse = {};
+
+    ajax.get(venueUrl).then(response => {
+        apiResponse = response.data.response.venue;
+        console.log(apiResponse);
+
+    }).then(() => {
+        // Render results page
+        res.render('venue', {
+            venue: apiResponse,
         });
+
+    }).catch(error => {
+        //console.log(error);
+        return res.send('Error fetching API data');
     });
 
-    app.get('/profile', isLoggedIn, function (req, res) {
-        res.render('profile', {
-            user: req.user // get the user out of session and pass to template
-        });
-    });
+});
 
-    app.get('/logout', function (req, res) {
-        req.logout();
+
+app.get('/login', function (req, res) {
+    res.render('login', {
+        message: req.flash('loginMessage')
+    });
+});
+
+app.post('/login', passport.authenticate('local-login', {
+        failureRedirect: '/login',
+        successRedirect: '/profile',
+        failureFlash: true // Allow flash messages
+    }),
+    function (req, res) {
+        if (req.body.remember) {
+            req.session.cookie.maxAge = 1000 * 60 * 3;
+        } else {
+            req.session.cookie.expires = false;
+        }
         res.redirect('/');
     });
 
-    // Return 404 on missing pages
-    app.get('*', function (req, res) {
-        res.status(404).send('Error: 404. Page not found !');
+app.get('/signup', function (req, res) {
+    res.render('signup', {
+        message: req.flash('signupMessage')
     });
+});
+
+app.post(
+    '/signup',
+    // Form filter and validation        
+    form(
+        field("firstName").trim().required().is(/^[A-z]+$/),
+        field("lastName").trim().required().is(/^[A-z]+$/),
+        field("password").trim().required().len(8, 72, "Password must be between 8 and 72 characters"),
+        field("email").trim().required().isEmail(),
+        validate("rpassword").equals("field::password")
+    ),
+
+    // Express request-handler now receives filtered and validated data 
+    function (req, res) {
+        // Additional validations            
+        req.body.email = req.body.email.toLowerCase();
+
+        if (!req.form.isValid) {
+            // Handle errors 
+            //console.log(req.form.errors);
+            //TODO: flash messages
+            res.redirect('/signup');
+
+        } else {
+            passport.authenticate('local-signup', {
+                successRedirect: '/login',
+                failureRedirect: '/signup',
+                failureFlash: true
+            })(req, res);
+        }
+    }
+);
+
+app.get('/error', function (req, res) {
+    res.render('error', {
+        message: req.flash('errorMessage')
+    });
+});
+
+app.get('/profile', isLoggedIn, function (req, res) {
+    res.render('profile', {
+        user: req.user // get the user out of session and pass to template
+    });
+});
+
+app.get('/logout', function (req, res) {
+    req.logout();
+    res.redirect('/');
+});
+
+// Return 404 on missing pages
+app.get('*', function (req, res) {
+    res.status(404).send('Error: 404. Page not found !');
+});
 
 };
 
